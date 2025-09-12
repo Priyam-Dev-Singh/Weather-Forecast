13a6e57df2fcf7c2160d67dd1186d8b35524be1d
Priyam <priyamdevsingh9@gmail.com>
2025-09-12 22:12:45 +0530

    bug fixes and improved UI

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let bg = document.getElementById("bg");
 let date = document.getElementById("date");
 let time = document.getElementById("time");
-
+let input = document.querySelector("#search input");
+let search = document.querySelector("#searchicon i");
+let weather = document.getElementById("weather");
 
 let day = new Date().getDay();
-let minutes= new Date().getMinutes();
-let hr = new Date().getHours();
+
 function daysletter(){
+
 if(day==0) day="SUNDAY";
 if(day==1) day="MONDAY";
 if(day==2) day="TUESDAY";
@@ -19,6 +21,8 @@ daysletter();
 
 date.innerHTML = `<p class="dt">${new Date().toLocaleDateString()}</p><p class="dt">${day}</p>`;
 function Time(){
+    let minutes= new Date().getMinutes();
+    let hr = new Date().getHours();
     time.innerHTML = `<p class="dt">${hr} : ${minutes} </p>`
     if(minutes==0) minutes="00";
     if(minutes<10) minutes=`0${minutes}`;
@@ -26,7 +30,23 @@ function Time(){
     if(hr<10) hr=`0${hr}`;
 }
 Time();
-setInterval(Time(), 500);
+setInterval(Time,1000);
 
-let citites=["Delhi","Mumbai","Chennai","Kolkata","Bangalore","Hyderabad","Pune","Ahmedabad","Jaipur","Lucknow","Kanpur","Nagpur","Indore","Thane","Bhopal","Visakhapatnam","Patna","Vadodara","Ghaziabad","Ludhiana","Agra","Nashik"];
+let cities=["delhi","mumbai","chennai","kolkata","bangalore","hyderabad","pune","ahmedabad","jaipur","lucknow","kanpur","nagpur","indore","thane","bhopal","visakhapatnam","patna","vadodara","ghaziabad","ludhiana","agra","nashik"];
+
+search.addEventListener("click", function(){
+    let cityName= input.value.toLowerCase();
+   if(cities.includes(cityName)) 
+    console.log(`city is found`);
+    else console.log(`city is not found`);
+} )
+
+input.addEventListener("keydown",function(event){
+    if(event.key==="Enter"){
+        let cityName= input.value.toLowerCase();
+        if(cities.includes(cityName)) 
+        console.log(`city is found`);
+        else console.log(`city is not found`);
+    }
+})
 
